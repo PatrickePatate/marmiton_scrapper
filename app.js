24108ce162d230107bfee33f6fbf79b83d34a0a3
PatrickePatate <lucienpuget1@gmail.com>
2024-09-27 14:08:14 +0200

    WIP: base commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import { PlaywrightCrawler, Dataset } from 'crawlee';
+
+// PlaywrightCrawler crawls the web using a headless
+// browser controlled by the Playwright library.
+const crawler = new PlaywrightCrawler({
+    // Use the requestHandler to process each of the crawled pages.
+    async requestHandler({ request, page, enqueueLinks, log }) {
+
+        const title = await page.title();
+        log.info(`Title of ${request.loadedUrl} is '${title}'`);
+
+        // Save results as JSON to ./storage/datasets/default
+        await Dataset.pushData({ title, url: request.loadedUrl });
+
+        // Extract links from the current page
+        // and add them to the crawling queue.
+        await enqueueLinks({
+            regexps: [new RegExp('https:\/\/www\.marmiton\.org\/recettes\/recette_([A-Za-z0-9\-\_]+).aspx')]
+        });
+    },
+    // Uncomment this option to see the browser window.
+    // headless: false,
+});
+
+// Add first URL to the queue and start the crawl.
+await crawler.run(['https://www.marmiton.org/recettes/recette_chou-campagnard_12832.aspx#d58611-p3']);
